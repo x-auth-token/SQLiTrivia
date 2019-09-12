c7fcf9992f5980ce6c0b7a455f817e0a4a173e3a
sh1ft <pmayzenberg@gmail.com>
2019-09-12 20:39:38 +0300

    Gui is done

--- app/src/main/java/com/pm/sqlitrivia/MainActivity.java
+++ app/src/main/java/com/pm/sqlitrivia/MainActivity.java
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.sqlitrivia.R;
+import com.pm.sqlitrivia.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/pm/sqlitrivia/SQLiTrivia.java
@@ -0,0 +1,14 @@
+package com.pm.sqlitrivia;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class SQLiTrivia extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
